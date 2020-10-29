e669ac8d33065b51cd32d8c4f0ec9917be64e107
SethMacP <macpherson.seth@gmail.com>
2020-10-29 14:52:45 -0700

    MVP Met

--- client/src/Movies/SavedList.js
+++ client/src/Movies/SavedList.js
@@ -1,13 +1,19 @@
 import React from 'react';
+import {Link , BrowserRouter, Switch, Route} from 'react-router-dom'
+import MovieList from './MovieList'
 
 export default function SavedList(props) {
+  console.log('props',props)
   return (
     <div className="saved-list">
       <h3>Saved Movies:</h3>
       {props.list.map(movie => (
         <span className="saved-movie">{movie.title}</span>
       ))}
-      <div className="home-button">Home</div>
+      <Link to='/' className="home-button">Home</Link>
+      
     </div>
   );
 }
+{/* <Route path ='/' render= {(props) => 
+          <MovieList movies = {props.movieList} />} /> */}
